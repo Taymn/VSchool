d338068300af001e96fbab5fedee284d0de85fd1
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-07-08 16:02:23 -0500

    modify element text lesson complete

---
+++ level1/mod-element-text/app.js
@@ -0,0 +1,17 @@
+// innerHTML - security risk
+// innerText - "expensive"
+// textContent
+
+// var person = {name: "Joe"}
+// person.name = "Jim"
+// console.log(person)
+
+/**
+ * 1. Select the h1 element
+ * 2. Save the h1 element's text to a variable
+ * 3. Change the paragraph element's text to be the same as the text inside the h1
+ */
+
+var text = document.querySelector("#header").textContent
+document.querySelector("#paragraph").textContent = text
+
