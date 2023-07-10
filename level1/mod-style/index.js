daf14aece1d751cf058647c070b84a67b06e56e5
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-07-10 11:46:53 -0500

    modify element style lesson complete

---
+++ level1/mod-style/index.js
@@ -0,0 +1,15 @@
+var header = document.getElementById("header")
+// console.log(typeof header.style)
+// console.log(Object.keys(header.style))
+header.style.color = "blue"
+document.body.style.backgroundColor = "burlywood"
+
+var h2 = document.getElementById("h2")
+h2.style.color = "green"
+h2.style.fontSize= "50px"
+
+// fontSize
+// backgroundColor
+// display
+// textAllign
+// margin, padding, border
