bf657e6c736c59da6ced8c46c02d16cf2b077171
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-07-11 18:47:16 -0500

    Challenge accepted and DOM review started

--- Code-Challenges/removeDupicates[Array]/index.js
+++ Code-Challenges/removeDupicates[Array]/index.js
@@ -1,14 +1,15 @@
-var arr = [1, 2, 3, 4, 4, 5, 6, 2, 3]
-var inst = [1,2,3,4,5,6]
+var arr = [1, 2, 3, 4, 4, 5, 6, 2, 3];
 
 function removeDuplicates(arr) {
     // Your code 
-    arr.forEach(element => {
-        if (!inst.includes(element)) {
-            inst.push(element);
-        }
-    });
-  }
+    let num = [];
+    for ( i = 0; i < arr.length; i++) {
+    if (num.indexOf(arr[i]) === -1) {
+        num.push(arr[i]);
+   }
+ }
+ return num;
+}
   
-  console.log(removeDuplicates([1, 2, 3, 4, 4, 5, 6, 2, 3])); // Expected Output: [1, 2, 3, 4, 5, 6]
-  console.log(removeDuplicates([10, 20, 30, 20, 40, 10])); // Expected Output: [10, 20, 30, 40]
+console.log(removeDuplicates([1, 2, 3, 4, 4, 5, 6, 2, 3])); /*Expected Output: [1, 2, 3, 4, 5, 6]*/
+console.log(removeDuplicates([10, 20, 30, 20, 40, 10]));    /* Expected Output: [10, 20, 30, 40]*/
---
+++ level1/dom-review/index.js
@@ -0,0 +1,7 @@
+/**
+ * CHALLENGE:
+ * 
+ * Play around with all the different DOM methods for selecting and modifying elements.
+ * 
+ * Make sure to read through the `notes.md` file for a recap and some new information regarding using the DOM API
+ */
