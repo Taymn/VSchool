f8864806d99f53c129f2dd182e8f5f29f795f826
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-07-12 14:40:42 -0500

    DOM reviewed, selfStudy begin

--- level1/dom-review/index.js
+++ level1/dom-review/index.js
@@ -5,3 +5,40 @@
  * 
  * Make sure to read through the `notes.md` file for a recap and some new information regarding using the DOM API
  */
+
+// selecting.Elements
+
+var header1 = document.getElementById("header1")
+header1.textContent = "not a header"
+console.log(header1);
+
+var list = document.querySelector("ol#list-items > li")
+console.log(list);
+
+var list = document.querySelectorAll("ol#list-items > li")
+console.log(Array.from(list))
+
+for (var i = 0; i< list.length; i++) {
+    list[i].textContent = "** REDACTED **"
+}
+
+var items = document.getElementsByClassName("list-items")
+for (var i = 0; i < items.length; i++) {
+console.log(items[i].innerText)
+}
+
+// modding.Elements
+
+var text = document.querySelector("ol#list-items > li").textContent
+document.querySelector("#header2").textContent = text
+
+var myList = document.getElementById("navbar")
+// console.log(myList.textContent)
+// console.log(myList.innerHTML)
+myList.innerHTML += "<li>querySelectorAll 4</li>"
+var navbar = document.getElementById("navbar")
+
+// console.log(typeof navbar.style)
+navbar.style.paddingTop = "16px"
+document.body.style.backgroundColor = "lime"
+
