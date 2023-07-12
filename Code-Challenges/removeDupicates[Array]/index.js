03f63309d10ecac75db7fb2028490a63f831e225
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-07-11 19:51:03 -0500

    Code Challenge submitted and reviewed

--- Code-Challenges/removeDupicates[Array]/index.js
+++ Code-Challenges/removeDupicates[Array]/index.js
@@ -10,6 +10,20 @@ function removeDuplicates(arr) {
  }
  return num;
 }
+
+// function removeDuplicates(arr){
+//     const result = []
+//     for (i = 0; i < arr.length; i++){
+//         if(!result.includes(arr[i])){
+//             result.push(arr[i])
+//         }
+//     }
+//     return result
+// }
+
+// function removeDuplicates(arr){
+//     return [...new Set( arr) ]
+// }
   
 console.log(removeDuplicates([1, 2, 3, 4, 4, 5, 6, 2, 3])); /*Expected Output: [1, 2, 3, 4, 5, 6]*/
 console.log(removeDuplicates([10, 20, 30, 20, 40, 10]));    /* Expected Output: [10, 20, 30, 40]*/
