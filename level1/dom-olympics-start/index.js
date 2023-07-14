53f04702531cc18c80fe85f6900a4863a24b7500
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-07-14 18:39:14 -0500

    DOM Olympics worked on, Gold left

--- level1/dom-olympics-start/index.js
+++ level1/dom-olympics-start/index.js
@@ -1,4 +1,4 @@
-//Peliminaries
+//Bronze
 
 const h1 = document.createElement("h1");
 h1.textContent = "JavaScript made this!!";
@@ -6,11 +6,15 @@ document.getElementById("header").appendChild(h1);
 h1.className = "header";
 
 const subTitle = document.createElement("span");
-document.getElementById("body").appendChild(subTitle);
-subTitle.innerHTML = 'Adam wrote the JavaScript';
+document.getElementById("header").appendChild(subTitle);
+subTitle.innerHTML = '<span class= "name">Adam</span> wrote the JavaScript';
+subTitle.className = "header";
+subTitle.style.textAlign = "right";
 
 
-//Bronze
+
+
+//Silver
 
 const messages = document.getElementsByClassName("message");
 
@@ -20,5 +24,73 @@ messages[2].textContent = "you're welcome";
 messages[3].textContent = "you're great";
 
 document.getElementById("clear-button").addEventListener('click', function (){
-    console.log("click me")
-})
+   for (var i = 0; i < messages.length; i--) {
+    messages[i].remove();
+}})
+
+/* <select id="theme-drop-down">
+<option value="theme-one">blue/brown</option>
+<option value="theme-two">red/black</option>
+<div class="message right">
+ <div class="message left">
+ .right {
+    align-self: flex-end;
+    background-color: lightblue;
+}
+
+.left {
+    align-self: flex-start;
+    background-color: burlywood;
+}
+</select> */
+
+document.getElementById("theme-drop-down").addEventListener('click', function(){
+    console.log("Click Me!")
+    if (document.getElementById("theme-drop-down").value === 'theme-one') { 
+        document.getElementById('first message left').style.backgroundColor = "burlywood";
+        document.getElementById('first message right').style.backgroundColor = "lightblue";
+        document.getElementById('second message left').style.backgroundColor = "burlywood";
+        document.getElementById('second message right').style.backgroundColor = "lightblue";
+        // console.log("Theme 1")
+
+    }else if (document.getElementById("theme-drop-down").value === 'theme-two') {
+        document.getElementById('first message left').style.backgroundColor = "red";
+        document.getElementById('first message right').style.backgroundColor = "black";
+        document.getElementById('second message left').style.backgroundColor = "red";
+        document.getElementById('second message right').style.backgroundColor = "black";
+        // console.log("Theme 2")
+
+        // /*Not working by Class Name (left and right)
+    
+    // }else if (document.getElementById("theme-drop-down").value === 'theme-two') {
+    //     document.getElementsByClassName('div.className.left').style.backgroundColor = "red";
+    //     document.getElementsByClassName('div.className.right').style.backgroundColor = "black";
+    //     document.getElementsByClassName('div.className.left').style.backgroundColor = "red";
+    //     document.getElementsBy('div.className.right').style.backgroundColor = "black";
+    //     console.log("Theme 2")*/
+    }})
+
+
+    var left = document.getElementsByClassName("left")
+    for (var i = 0; i < left.length; i++) {
+        console.log(left[i].innerText)
+    //     if (function () {
+    //         left.style.backgroundColor = "burlywwod")
+    //     }else if (function () {
+    //         left.style.backgroundColor = "burlywwod")
+         
+    //     })
+     }
+
+    var right = document.getElementsByClassName("right")
+    for (var i = 0; i < right.length; i++) {
+        console.log(right[i].innerText)
+    //     if (function () {
+    //         left.style.backgroundColor = "lightblue";
+    //     }else if (function ()) {
+    //         left.style.backgroundColor = "black";
+    //     })
+     }
+
+    //Gold
+
--- level1/workshop/main.js
+++ level1/workshop/main.js
@@ -1,8 +1,8 @@
 const readline = require('readline-sync');
 
-// const playerName = readline.question("Hello, what is your name?")
+const playerName = readline.question("Hello, what is your name?")
 
-// console.log(`Welcome ${playerName} to the RPG game`);
+console.log(`Welcome ${playerName} to the RPG game`);
 
 // function Character(name, healthPoints, attackPoints, inventory){
 //     this.name = name
@@ -29,20 +29,20 @@ class Character{
 }
 
 
-// let playerInventory = ["bow","sword","axe","rocket launcher"];
+let playerInventory = ["bow","sword","axe","rocket launcher"];
 
-// const hero = new Character(playerName, 100, 5,["bow","sword","axe","rocket launcher"]);
+const hero = new Character(playerName, 100, 5,["bow","sword","axe","rocket launcher"]);
 
-const enemy1 = new Character("Tricky Tim", 25, 2, ["Tooth", "Bracelet"]);
-const enemy2 = new Character("Lulu", 36, 3, ["Sword", "Necklace"]);
-const enemy3 = new Character("Mickey", 18, 1, ["Ears"]);
+// const enemy1 = new Character("Tricky Tim", 25, 2, ["Tooth", "Bracelet"]);
+// const enemy2 = new Character("Lulu", 36, 3, ["Sword", "Necklace"]);
+// const enemy3 = new Character("Mickey", 18, 1, ["Ears"]);
 
-// console.log(hero);
+ console.log(hero);
 // console.log(enemy1);
 // console.log(enemy2);
 // console.log(enemy3);
 
-let enemies = [enemy1, enemy2, enemy3]
+// let enemies = [enemy1, enemy2, enemy3]
 
-console.log(enemies);
+// console.log(enemies);
 // hero.printInventory();
