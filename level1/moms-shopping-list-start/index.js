ac2211cf2c87c22c3d0b24dbd344995aec1edff7
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-07-19 19:44:59 -0500

    MSL completed - Parts 1 & 2, no EC

--- level1/moms-shopping-list-start/index.js
+++ level1/moms-shopping-list-start/index.js
@@ -5,24 +5,37 @@ let title = document.getElementById("title").value; /*input field*/
 /* Submit Event */
 
 form.addEventListener("submit", (event)=>{
-    console.log("help")
 
     event.preventDefault()/*prevents page refresh*/
 
     let input = document.getElementById("title").value;
     document.getElementById("title").value = ""
     var newLi = document.createElement("li")
+    var newDiv = document.createElement("div")
+    
     var edBtn = document.createElement("button")
+    edBtn.innerText ="edit"
+    edBtn.setAttribute("class", "editButton")
+
+    edBtn.addEventListener("click", function(){
+        newLi.inputMode()
+    })
+
     var deleBtn = document.createElement("button")
+    deleBtn.innerText = "X"
+    deleBtn.setAttribute("class", "deleteButton")
+
+    // removes parent and children
+    deleBtn.addEventListener("click", function(){
+        newLi.remove()
+    })
 
     newLi.textContent = input
-    newLi.setAttribute("class","#list")
-    list.append(newLi)  
-    deleBtn.setAttribute("class","#list")
-    deleBtn.appendChild(document.createTextNode(input.value))
-    edBtn.setAttribute("class","#list")
-    edBtn.appendChild(document.createTextNode(input.value))
+    newLi.setAttribute("class","listItem")
+    newDiv.setAttribute("class","listContainer")
 
-   
-
+    list.appendChild(newLi)
+    newLi.appendChild(newDiv)
+    newDiv.appendChild(edBtn)
+    newDiv.appendChild(deleBtn)
 })
