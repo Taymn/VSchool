3cc3bd3f28b5403dcde73c4d07869ba1591a5e1b
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-08-16 20:34:41 -0500

    Mario Assignment completed

--- level2/Mario-pest-control/index.js
+++ level2/Mario-pest-control/index.js
@@ -1,8 +1,26 @@
-var total =  document.getElementById('total')
-var totalAmount = 0
-total.textContent = totalAmount
+const addBaddie = document.getElementById("submit")
 
-totalAmount += 2
+addBaddie.addEventListener("click",()=>{
 
-total.textContent = totalAmount
-//add button to clear total
+//const for 3 baddies to pull values
+
+const Goombas = document.getElementById("goombasInput").value
+console.log(Goombas)
+const Bobboms = document.getElementById("bobbomsInput").value
+console.log(Bobboms)
+const Cheeps = document.getElementById("cheepsInput").value
+console.log(Cheeps)
+
+//create element to display values as a total
+
+var total = document.getElementById('total')
+var totalAmount = Goombas*5 + Bobboms*7 + Cheeps*11
+
+//add to the textContent
+
+total.textContent = "Total: "+totalAmount
+
+// totalAmount += 2
+
+console.log(totalAmount)
+})
