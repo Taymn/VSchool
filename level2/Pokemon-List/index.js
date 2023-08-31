ba6e142dd1e27b0e8d96936dece5b7552b4cc9b3
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-08-31 10:45:57 -0500

    Pokemon-List updated with details

--- level2/Pokemon-List/index.js
+++ level2/Pokemon-List/index.js
@@ -17,7 +17,10 @@ xhr.onreadystatechange = function () {
 function showData(arr) {
     for (let i = 0; 0 < arr.length; i++) {
         const h1 = document.createElement('h1')
+        const h2 = document.createElement('h2')
         h1.textContent = arr[i].name
         document.body.appendChild(h1)
+        h2.textContent = arr[i].resource_uri
+        document.body.appendChild(h2)
     }
 }
