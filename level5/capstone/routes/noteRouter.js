0c4d24ab7c5dd48757e8d8ddb6b8bf3cd4e10792
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-11-02 16:57:34 -0500

    Backend of Capstone 5 reached MVP

--- level5/capstone/routes/noteRouter.js
+++ level5/capstone/routes/noteRouter.js
@@ -4,7 +4,7 @@ const Note = require('../models/note.jsx')
 
 noteRouter.get('/', (req, res, next) => {
     Note.find((err, notes) => {
-        if(err) {
+        if (err) {
             res.status(500)
             return next(err)
         }
@@ -12,30 +12,68 @@ noteRouter.get('/', (req, res, next) => {
     })
 })
 
+noteRouter.get('/:noteID', (req, res, next) => {
+    const noteID = req.params.noteID
+    const foundNote = notes.find(note => note._id === noteID);
+    if (!foundNote) {
+        const error = new Error(`The note with id ${noteID} was not found.`);
+        res.status(500)
+        return next(error)
+    }
+    res.status(200).send(foundNote);
+})
+
+
 noteRouter.post('/', (req, res, next) => {
-    const newNote = new Note(req.body)
-    newNote.save((err, savedNote) =>{
-        if(err) {
+    const newNote = new Note(...req.body)
+    newNote.save((err, savedNote) => {
+        if (err) {
             res.status(500)
             return next(err)
         }
         return res.status(201).send(savedNote)
-    }) 
+    })
 })
 
 noteRouter.put('/like/:noteID', (req, res, next) => {
     Note.findOneAndUpdate(
         { _id: req.params.noteID },
-        { $inc: {likes: 1}},
+        { $inc: { likes: 1 } },
         { new: true },
         (err, updatedNote) => {
-            if(err){
+            if (err) {
                 res.status(500)
                 return next(err)
             }
             return res.status(201).send(updatedNote)
         }
     )
+})
+
+noteRouter.put('/dislike/:noteID', (req, res, next) => {
+    Note.findOneAndUpdate(
+        { _id: req.params.noteID },
+        { $inc: { dislikes: 1 } },
+        { new: true },
+        (err, updatedNote) => {
+            if (err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(201).send(updatedNote)
+        }
+    )
+})
+
+noteRouter.delete('/:noteID', (req, res, next) => {
+    Note.findOneAndDelete({ _id: req.params.noteID }, (err, deletedItem) => {
+        if (err) {
+            res.status(500)
+            return next(err)
+
+        }
+        return res.status(200).send(`Successfully deleted item ${deletedItem.title} from database`)
+    })
 })
 
 module.exports = noteRouter
