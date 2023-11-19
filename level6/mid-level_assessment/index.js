83fdad61dce5626842e04134d0cc0f5e74061d40
ADAM TAYLOR <adam_gt2003@yahoo.com>
2023-11-19 16:22:31 -0600

    Mid Level Assessment Completed

--- level6/mid-level_assessment/index.js
+++ level6/mid-level_assessment/index.js
@@ -17,7 +17,7 @@ function extractInitials(names) {
   };
 
 
-const fullNames = ['John Doe', 'Alice Johnson', 'Bob Smith'];
+const fullNames = ['John Doe', 'Alice Tori Johnson', 'Bob Smith'];
 const initialsArray = extractInitials(fullNames);
 console.log(initialsArray); // Output: ['JD', 'AJ', 'BS']
 // ```jsx
@@ -38,3 +38,31 @@ const people = [
 
 const filteredByCountry = filterByProperty(people, 'country', 'USA');
 console.log(filteredByCountry);
+
+// Write a function called createTable(rows, columns) that takes two parameters: rows (number of rows) and columns (number of columns). The function should generate a table represented as an array of arrays, where each inner array represents a row and contains the row's data. Each cell in the table should contain the product of its row index and column index.
+function createTable(rows, columns) {
+  let table = []
+  for ( let i = 0; i < rows; i++ ){
+    const row = []
+    for ( let j =0; j< columns; j++){
+      row.push(i * j)
+    }
+    table.push(row)
+  }
+  return table
+}
+
+// Test the function with different values
+const table1 = createTable(3, 4);
+console.log(table1);
+
+/* Expected Outcome:
+[
+  [0, 0, 0, 0],
+  [0, 1, 2, 3],
+  [0, 2, 4, 6]
+]
+ */
+
+const table2 = createTable(5, 5);
+console.log(table2);
